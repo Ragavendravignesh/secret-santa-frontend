4898295845af0ab55a63e4bd6aed7885e8960297
Ragavendra S <ragavendrabest77@gmail.com>
2021-12-27 12:46:25 +0530

    styling changes added

--- pages/404.js
+++ pages/404.js
@@ -5,10 +5,10 @@ export default function PageNotFound() {
   return (
     <Layout title={'Page not found'}>
       <div className={styles.container}>
-        <h1 className='title'>
-          <span style={{color:'#ff0000'}}>404</span> Not found
+        <h1 className={styles.title}>
+          <span className={styles.redText}>404</span> Not found
         </h1>
-        <h4 className='content'>
+        <h4 className={styles.content}>
           Sorry, at the moment we couln't find the page you are looking for.
         </h4>
       </div>
--- pages/login.js
+++ pages/login.js
@@ -1,33 +1,47 @@
-import { useState } from 'react';
-import Layout from '../components/Layout';
-import { FaMale } from 'react-icons/fa';
-import styles from '../styles/login.module.css';
+import { useState } from 'react'
+import Layout from '../components/Layout'
+import { FaHandHoldingHeart } from 'react-icons/fa'
+import styles from '../styles/login.module.css'
+import Link from 'next/link'
 
 export default function loginPage() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-        console.log(email, password);
-    }
+    console.log(email, password)
+  }
 
-    return (
-       <Layout title={'Login page'}>
-           <div className={styles.card}>
-               <div className={styles.logo}>
-                   <FaMale/>
-               </div>
-               <div className={styles.form}>
-                   <form onSubmit={handleSubmit}>
-                        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+  return (
+    <Layout title={'Login page'}>
+      <div className={styles.card}>
+        <p className={styles.registerTxt}>
+          <Link href={'/register'}>
+            Register
+          </Link>
+        </p>
+        <div className={styles.logo}>
+          <FaHandHoldingHeart />
+        </div>
+        <div className={styles.form}>
+          <form onSubmit={handleSubmit}>
+            <input
+              type='email'
+              placeholder='Email'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type='password'
+              placeholder='Password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-                        <input type='submit' value='Login' className={styles.authBtn}/>
-                   </form>
-               </div>
-           </div>
-       </Layout> 
-    )
+            <input type='submit' value='Login' className={styles.authBtn} />
+          </form>
+        </div>
+      </div>
+    </Layout>
+  )
 }
